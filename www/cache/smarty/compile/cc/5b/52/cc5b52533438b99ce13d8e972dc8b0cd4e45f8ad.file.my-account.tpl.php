ce90c56a5910e58e137a261545f31567494704af
nickniebaum <nickniebaum@gmail.com>
2013-08-26 13:16:39 -0500

    Center logo, reposition account wrapper

--- www/cache/smarty/compile/cc/5b/52/cc5b52533438b99ce13d8e972dc8b0cd4e45f8ad.file.my-account.tpl.php
+++ www/cache/smarty/compile/cc/5b/52/cc5b52533438b99ce13d8e972dc8b0cd4e45f8ad.file.my-account.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2013-08-07 16:55:02
+<?php /* Smarty version Smarty-3.1.13, created on 2013-08-08 13:25:20
          compiled from "/Users/nick/Desktop/projects/Repositories/doubletuff/www/themes/theme740/modules/favoriteproducts/my-account.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:4573082555202c236ea5ce7-54082656%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'cc5b52533438b99ce13d8e972dc8b0cd4e45f8ad' => 
     array (
       0 => '/Users/nick/Desktop/projects/Repositories/doubletuff/www/themes/theme740/modules/favoriteproducts/my-account.tpl',
-      1 => 1375913100,
+      1 => 1375986286,
       2 => 'file',
     ),
   ),
@@ -15,22 +15,22 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_5202c236ef0404_22972726',
   'variables' => 
   array (
     'link' => 0,
     'in_footer' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.13',
-  'unifunc' => 'content_5202c236ef0404_22972726',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_5202c236ef0404_22972726')) {function content_5202c236ef0404_22972726($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_escape')) include '/Users/nick/Desktop/projects/Repositories/doubletuff/www/tools/smarty/plugins/modifier.escape.php';
 ?>
 
 <li class="favoriteproducts">
 	<a href="<?php echo smarty_modifier_escape($_smarty_tpl->tpl_vars['link']->value->getModuleLink('favoriteproducts','account'), 'htmlall', 'UTF-8');?>
-" title="<?php echo smartyTranslate(array('s'=>'My favorite products.','mod'=>'favoriteproducts'),$_smarty_tpl);?>
-"><?php if (!$_smarty_tpl->tpl_vars['in_footer']->value){?><i class="icon-heart"></i><?php }else{ ?><i class="icon-circle-arrow-right"></i><?php }?><?php echo smartyTranslate(array('s'=>'My favorite products.','mod'=>'favoriteproducts'),$_smarty_tpl);?>
+" title="<?php echo smartyTranslate(array('s'=>'My favorite products','mod'=>'favoriteproducts'),$_smarty_tpl);?>
+"><?php if (!$_smarty_tpl->tpl_vars['in_footer']->value){?><i class="icon-heart"></i><?php }else{ ?><i class="icon-circle-arrow-right"></i><?php }?><?php echo smartyTranslate(array('s'=>'My favorite products','mod'=>'favoriteproducts'),$_smarty_tpl);?>
 </a>
 </li>
 <?php }} ?>
